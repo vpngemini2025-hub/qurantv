984c8f88ad9bb88084b73961ea5d271a1f7c69af
vpngemini2025-hub <vpngemini2025@gmail.com>
2026-01-06 21:35:42 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'jellouli-quran-v1';
+const assetsToCache = [
+  './index.html',
+  './manifest.json',
+  './logo-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
